1815b1f4d4f1569da5ff98eaff2cad745b8426c7
Mosae-Litsoane <mosaelitsoane@gmail.com>
2021-03-11 01:01:31 -0600

    Need to fix hooks

--- src/components/cards/card.tsx
+++ src/components/cards/card.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { makeStyles, Theme, createStyles } from '@material-ui/core/styles';
 import clsx from 'clsx';
 import Card from '@material-ui/core/Card';
@@ -43,7 +43,7 @@ const useStyles = makeStyles((theme: Theme) =>
 
 export default function card() {
 	const classes = useStyles();
-	const [expanded, setExpanded] = React.useState(false);
+	const [expanded, setExpanded] = useState(false);
 
 	const handleExpandClick = () => {
 		setExpanded(!expanded);
