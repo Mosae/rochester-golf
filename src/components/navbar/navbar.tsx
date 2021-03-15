cfe786790c3cc89d3a3999330db06b841e0016e9
Mosae-Litsoane <mosaelitsoane@gmail.com>
2021-03-15 18:34:25 -0500

    Card shows with image for course

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -3,8 +3,18 @@ import './navbar.css';
 function navbar() {
 	return (
 		<div className="navbar__wrapper">
-			<p className="navbar__logo">Rochester Golf</p>
+			<ul className="nav__ul">
+				<li>
+					<a href="/">Rochester Golf</a>
+				</li>
+				<li className="nav__right">
+					<a href="/">Contact us</a>
+				</li>
+			</ul>
 		</div>
+		// <div className="navbar__wrapper">
+		// 	<p className="navbar__logo">Rochester Golf</p>
+		// </div>
 	);
 }
 
