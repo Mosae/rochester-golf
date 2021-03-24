64e7c7dc284260033f0e909be277ef94feab1ccc
Mosae-Litsoane <mosaelitsoane@gmail.com>
2021-03-23 23:30:38 -0500

    Added data file, looping over it. errors gone but minor bug

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Navbar from './components/navbar/navbar';
 import Header from './components/header/header';
-import Cards from './components/cards/card';
+import MediaCard from './components/cards/card';
 import Ft from './components/footer/Footer';
 import { Grid } from '@material-ui/core';
 
@@ -10,17 +10,8 @@ function main() {
 		<div>
 			<Navbar />
 			<Header />
-			<Grid container>
-				<Grid item xs={12} md={4} lg={4}>
-					<Cards />
-				</Grid>
-				<Grid item xs={12} md={4} lg={4}>
-					<Cards />
-				</Grid>
-				<Grid item xs={12} md={4} lg={4}>
-					<Cards />
-				</Grid>
-			</Grid>
+			<MediaCard />
+
 			<Ft />
 		</div>
 	);
